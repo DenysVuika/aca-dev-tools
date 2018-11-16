aad4fea0d7771a7a961df25c05a8943e9c1f551e
Denys Vuika <denys.vuika@gmail.com>
2018-11-16 16:06:13 +0000

    update dev tools

--- projects/aca-dev-tools/src/lib/aca-dev-tools.module.ts
+++ projects/aca-dev-tools/src/lib/aca-dev-tools.module.ts
@@ -5,12 +5,10 @@ import { AcaDevToolsComponent } from './aca-dev-tools.component';
 import { CoreModule } from '@alfresco/adf-core';
 import { ContentModule } from '@alfresco/adf-content-services';
 import { ExtensionService } from '@alfresco/adf-extensions';
-import { MarkdownViewComponent } from './viewer/markdown-view/markdown-view.component';
 
 export function components() {
     return [
-        AcaDevToolsComponent,
-        MarkdownViewComponent
+        AcaDevToolsComponent
     ];
 }
 
@@ -28,8 +26,7 @@ export function components() {
 export class AcaDevToolsModule {
     constructor(extensions: ExtensionService) {
         extensions.setComponents({
-            'dev.tools.component': AcaDevToolsComponent,
-            'dev.tools.viewer.markdown': MarkdownViewComponent
+            'dev.tools.component': AcaDevToolsComponent
         });
     }
 }
